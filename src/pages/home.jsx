c4c4bcc6ea34ce18762505f179bd2078136333c2
Ananthapadmanabhan333 <padmanabhan086@gmail.com>
2026-01-16 17:08:56 +0530

    Fix import case for Hero component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import Hero from "../components/hero";
+import Hero from "../components/Hero";
 import About from "../components/About";
 import Schedule from "../components/Schedule";
 import CTA from "../components/CTA";
